3d62fb077ad68eabdad6d2618e8fd31472b32c63
Shubham <shubhamsainikumar55123@gmail.com>
2022-09-13 13:57:57 +0530

    update

--- TestByAk/src/com/LearnJava/Program/Array.java
+++ TestByAk/src/com/LearnJava/Program/Array.java
@@ -7,34 +7,27 @@ public class Array {
 	public static void main(String[] args) {
 		//int rollNo[] = new int [5];
 		int rollNo[];
-		
+		int size;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Size of Array=");
-		int size = sc.nextInt();
-		
-		
+		size = sc.nextInt();
 		while(size<=0)
 		{
 			System.out.print("Enter Size of Array");
 			size = sc.nextInt();
 		}
 		rollNo = new int [size];
-		int value = 0;
-		for(int i=0; i<rollNo.length; i++)
+				
+		for(int i=0; i<size; i++)
 		{
-			for(i=0; i<rollNo.length; i++)
-			{
 			System.out.print("Enter RollNo["+i+"]=");
-			value = sc.nextInt();
-			
-			//System.out.println(i);
-							
-			}
-			System.out.println("RollNo["+i+"]="+value);
-			//System.out.println(i);
+			rollNo[i] = sc.nextInt();
 		}
-		
-		
+		for(int i=0; i<size; i++)
+		{
+			System.out.println("RollNo["+i+"]="+rollNo[i]);
+			
+		}
 //		System.out.println(rollNo[0]);
 //		System.out.println(rollNo[1]);
 //		System.out.println(rollNo[2]);
